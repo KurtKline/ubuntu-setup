3ecfc622cc0510cac4ead7ab934e12c9b62f8fef
Kurt Kline <kurtm.kline@gmail.com>
2020-04-29 01:57:32 +0200

    Update aptinstall.sh

--- scripts/aptinstall.sh
+++ scripts/aptinstall.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt full-upgrade -y
 
-function apt_install {
+function install {
   which $1 &> /dev/null
 
   if [ $? -ne 0 ]; then
@@ -13,27 +13,8 @@ function apt_install {
   fi
 }
 
-function snap_install {
-  which $1 &> /dev/null
-
-  if [ $? -ne 0 ]; then
-    echo "Installing: ${1}..."
-    sudo snap install $1 --classic
-  else
-    echo "Already installed: ${1}"
-  fi
-}
-
-# Only available with apt
-apt_install curl
-apt_install file
-apt_install git
-apt_install vim
-apt_install chromium-browser
-
-# Snap installs
-snap_install htop
-snap_install tree
-snap_install pycharm-community
-snap_install spotify
-snap_install atom
+install curl
+install file
+install git
+install vim
+install chromium-browser
