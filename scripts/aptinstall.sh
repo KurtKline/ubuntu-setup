7c5c16d6c68258b1d6b642e18694f7f6f574d457
Kurt Kline <kurtm.kline@gmail.com>
2020-04-28 23:55:13 +0200

    Update aptinstall.sh

--- scripts/aptinstall.sh
+++ scripts/aptinstall.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt full-upgrade -y
 
-function install {
+function basic_install {
   which $1 &> /dev/null
 
   if [ $? -ne 0 ]; then
@@ -13,10 +13,26 @@ function install {
   fi
 }
 
-# Programs
-install curl
-install file
-install git
-install htop
-install tree
-install vim
+function snap_install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo snap install $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# Only available with apt
+basic_install curl
+basic_install file
+basic_install git
+basic_install vim
+
+# Snap installs
+snap_install pycharm-community
+snap_install htop
+snap_install tree
+snap_install spotify
+snap_install sublime-text
