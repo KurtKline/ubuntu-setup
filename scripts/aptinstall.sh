c161d089278f817bb1890cc8e746de482836d3fd
Kurt Kline <kurtm.kline@gmail.com>
2020-04-28 22:50:19 +0200

    Create aptinstall.sh

---
+++ scripts/aptinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update && sudo apt full-upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# Programs
+install curl
+install file
+install git
+install htop
+install tree
+install vim
