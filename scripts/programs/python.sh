3df1a4ad0f9c08400b141b120c3a6bdad4ee92fe
Kurt Kline <kurtm.kline@gmail.com>
2020-04-28 23:09:24 +0200

    Update python.sh

--- scripts/programs/python.sh
+++ scripts/programs/python.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "🐍 Installing Python"
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install software-properties-common 
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install -y python3.8 python3-pip python3-venv
