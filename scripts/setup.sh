3bca5cdd480f72952c2637473d809894dd9fd05a
Kurt Kline <kurtm.kline@gmail.com>
2020-04-30 23:37:15 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,14 +4,12 @@
 ./aptinstall.sh
 ./snapinstall.sh
 ./programs.sh
+./desktop.sh
 
 # Get all upgrades
 sudo apt upgrade -y
 
 # See our bash changes
 source ~/.bashrc
-
-# Set favorites in dock
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'pycharm-community_pycharm-community.desktop', 'code_code.desktop', 'atom_atom.desktop', 'chromium_chromium.desktop', 'spotify_spotify.desktop', 'org.gnome.Terminal.desktop']"
 
 echo "System setup complete. Now enjoy your ☕"
