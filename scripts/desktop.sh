d03fe1bfa4906cbe4deb5725b7b18f764fed0141
Kurt Kline <kurtm.kline@gmail.com>
2020-05-01 00:35:36 +0200

    Update desktop.sh

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Customizing Desktop"
+echo "🖥️ Customizing Desktop"
 
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'pycharm-community_pycharm-community.desktop', 'code_code.desktop', 'atom_atom.desktop', 'chromium_chromium.desktop', 'spotify_spotify.desktop', 'org.gnome.Terminal.desktop']"
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
