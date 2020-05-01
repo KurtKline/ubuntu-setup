177555d79e34174acd18bbe99e1982b51e0bceec
Kurt Kline <kurtm.kline@gmail.com>
2020-05-01 17:15:13 +0200

    Update desktop.sh

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -2,6 +2,10 @@
 
 echo "🖥️ Customizing Desktop"
 
+# Installing Canta theme
+git clone https://github.com/vinceliuice/Canta-theme.git
+./install.sh -c dark -t standard -s standard
+
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'pycharm-community_pycharm-community.desktop', 'code_code.desktop', 'atom_atom.desktop', 'chromium_chromium.desktop', 'spotify_spotify.desktop', 'org.gnome.Terminal.desktop']"
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
