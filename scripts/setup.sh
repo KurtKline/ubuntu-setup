6d5682a89bd2dee0ea49467fa301def9b36210d7
Kurt Kline <kurtm.kline@gmail.com>
2020-05-01 23:10:11 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,16 +15,6 @@ source ~/.bashrc
 # Set-up complete
 figlet "Setup complete!" | lolcat
 
-# Restart Gnome shell, so that extensions can be shown
-function sleep_countdown() {
-	i=$@
-	while [ $i -gt 0 ]; do
-		echo "Restarting shell in $i seconds"
-		sleep 1
-		let i=i-1
-	done
-}
+# Restart Gnome shell so new extensions will appear
+./rebootshell.sh
 
-sleep_countdown 5
-
-# killall -1 gnome-shell
