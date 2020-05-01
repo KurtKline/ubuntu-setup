2fb2b87f5726d30a7da16d4bdb65d47e8003f663
Kurt Kline <kurtm.kline@gmail.com>
2020-05-01 14:05:41 +0200

    Update desktop.sh

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -5,4 +5,7 @@ echo "🖥️ Customizing Desktop"
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'pycharm-community_pycharm-community.desktop', 'code_code.desktop', 'atom_atom.desktop', 'chromium_chromium.desktop', 'spotify_spotify.desktop', 'org.gnome.Terminal.desktop']"
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
+gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark' 
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface show-battery-percentage true
