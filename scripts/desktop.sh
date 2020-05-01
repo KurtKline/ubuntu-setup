1ae1b4a1dfc6040885e4f596e59fe1b77e7b4b0f
Kurt Kline <kurtm.kline@gmail.com>
2020-05-01 18:19:21 +0200

    Update desktop.sh

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -8,9 +8,14 @@ mkdir ~/.themes/; mv Canta-theme $_
 cd ~/.themes/Canta-theme/ && ./install.sh -c dark -t standard -s standard
 
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'pycharm-community_pycharm-community.desktop', 'code_code.desktop', 'atom_atom.desktop', 'chromium_chromium.desktop', 'spotify_spotify.desktop', 'org.gnome.Terminal.desktop']"
+gsettings set org.gnome.shell enabled-extensions "['ubuntu-dock@ubuntu.com', 'user-theme@gnome-shell-extensions.gcampax.github.com']"
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
-gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark' 
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+gsettings set org.gnome.shell.extensions.user-theme name 'Canta-dark' # controls top bar, dock
+gsettings set org.gnome.desktop.interface gtk-theme 'Canta-dark' # controls overall theme of windows, terminals, etc. 
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
