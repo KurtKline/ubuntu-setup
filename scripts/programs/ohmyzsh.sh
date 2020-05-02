b0e0d4abda9492215207cd1e23e0bc3f2ed735f4
Kurt Kline <kurtm.kline@gmail.com>
2020-05-02 13:06:28 +0200

    Delete ohmyzsh.sh

--- scripts/programs/ohmyzsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing oh-my-zsh 🦁🐯🐻"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-# Set as default shell
-# chsh -s $(which zsh)
