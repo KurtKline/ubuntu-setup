9c4c85f15250d4c56bc9cfe8e275a484348ecdbc
Kurt Kline <kurtm.kline@gmail.com>
2020-05-02 12:01:28 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SECONDS=0
+
 ./symlink.sh
 ./aptinstall.sh
 ./snapinstall.sh
@@ -17,3 +19,17 @@ figlet "Setup complete!" | lolcat
 
 # See our bash changes
 source ~/.bashrc
+
+# Timing for set-up process
+if (( $SECONDS > 3600 )) ; then
+    let "hours=SECONDS/3600"
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Set-up completed in $hours hour(s), $minutes minute(s) and $seconds second(s)" 
+elif (( $SECONDS > 60 )) ; then
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Set-up completed in $minutes minute(s) and $seconds second(s)"
+else
+    echo "Set-up completed in $SECONDS seconds"
+fi
