88d6e7a0792cd157f410779c8a6c26a5c7139d38
Kurt Kline <kurtm.kline@gmail.com>
2020-05-02 13:06:33 +0200

    Delete vundle.sh

--- scripts/programs/vundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "📦 Installing Vundle"
-
-# Vundle plugin manager
-mkdir -p $HOME/.vim/bundle
-cd $HOME/.vim/bundle
-git clone git://github.com/VundleVim/Vundle.vim.git
-vim +PluginInstall +qall
